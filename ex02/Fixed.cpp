688ba5ac6abbc5fc3134bbf162d29126efa8ac89
Killian0b101010 <teixeira.killian44600@gmail.com>
2025-10-22 03:02:02 +0200

    merci doriane

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 21:09:35 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/10/21 04:04:00 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/10/22 02:48:45 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,19 @@
 // Valeurs initial de notre classe fixed a 0 c'est la valeurs qui
 // qui sera dans notre main et qu'on va pouvoir transporter dans la memoire en
 // int tous en castant  en float..
-Fixed::Fixed() {
-  this->_fixedVal = 0;
-  std::cout << "Default constructor called" << std::endl;
-}
+Fixed::Fixed() { this->_fixedVal = 0; }
 
 //  Construtcteur de copie tres utile pour copier une instance de classe(generer
 //  un autre objet)
-Fixed::Fixed(const Fixed &fixed) {
-  this->_fixedVal = fixed._fixedVal;
-  std::cout << "Copy constructor called" << std::endl;
-}
+Fixed::Fixed(const Fixed &fixed) { this->_fixedVal = fixed._fixedVal; }
 
 // Constructeur qui prend en parametre le type float il sera arrondis a 2
 // chiffre apres la virgule (8 bits) on decale de 1 pour la virgule.
-Fixed::Fixed(float number) {
-  std::cout << "Float constructor called" << std::endl;
-  _fixedVal = roundf(number * (1 << this->bits));
-}
+Fixed::Fixed(float number) { _fixedVal = roundf(number * (1 << this->bits)); }
 
 // Constructeur qui prend un simple int et qui le return simplement cette fois
 // ci on multiplie par 256
-Fixed::Fixed(int const fix) {
-  std::cout << "Int constructor called" << std::endl;
-  this->_fixedVal = fix << this->bits;
-}
+Fixed::Fixed(int const fix) { this->_fixedVal = fix << this->bits; }
 // ces la fonction qui nous permet de convertir un type float fixed en float..
 // on divise par 256 et on ce decale pour la virgule..
 float Fixed::toFloat(void) const {
@@ -49,18 +37,98 @@ float Fixed::toFloat(void) const {
 // on return juste un int simple
 int32_t Fixed::toInt(void) const { return (this->_fixedVal >> this->bits); }
 
-Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+Fixed::~Fixed() {}
 
-int Fixed::getRawBits() const {
-  std::cout << "getRawBits member function called" << std::endl;
-  return (this->_fixedVal);
-}
+int Fixed::getRawBits() const { return (this->_fixedVal); }
 
 Fixed &Fixed::operator=(const Fixed &other) {
 
-  std::cout << "Copy assignement operator called" << std::endl;
   this->_fixedVal = other.getRawBits();
   return (*this);
+}
+
+bool Fixed::operator<(const Fixed &other) const {
+  return (this->_fixedVal < other._fixedVal);
+}
+bool Fixed::operator>(const Fixed &other) const {
+  return (this->_fixedVal > other._fixedVal);
+}
+bool Fixed::operator>=(const Fixed &other) const {
+  return (this->_fixedVal >= other._fixedVal);
+}
+
+bool Fixed::operator<=(const Fixed &other) const {
+  return (this->_fixedVal <= other._fixedVal);
+}
+
+bool Fixed::operator==(const Fixed &other) const {
+  return (this->_fixedVal == other._fixedVal);
+}
+
+bool Fixed::operator!=(const Fixed &other) const {
+  return (this->_fixedVal != other._fixedVal);
+}
+
+Fixed Fixed::operator+(const Fixed &other) {
+  return (this->toFloat() + other.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed &other) {
+  return (this->toFloat() - other.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed &other) {
+  return (this->toFloat() * other.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed &other) {
+  return (this->toFloat() / other.toFloat());
+}
+
+Fixed &Fixed::operator++() {
+  this->_fixedVal++;
+  return (*this);
+}
+Fixed &Fixed::operator--() {
+  this->_fixedVal--;
+  return (*this);
+}
+Fixed Fixed::operator++(int) {
+  Fixed temp(*this);
+  this->_fixedVal++;
+  return (temp);
+}
+
+Fixed Fixed::operator--(int) {
+  Fixed temp(*this);
+  this->_fixedVal--;
+  return (temp);
+}
+
+Fixed Fixed::max(Fixed &a, Fixed &b) {
+  if (a > b)
+    return (a);
+  else
+    return (b);
+}
+
+Fixed Fixed::max(const Fixed &a, const Fixed &b) {
+  if (a > b)
+    return (a);
+  else
+    return (b);
+}
+Fixed Fixed::min(Fixed &a, Fixed &b) {
+  if (a < b)
+    return (a);
+  else
+    return (b);
+}
+Fixed Fixed::min(const Fixed &a, const Fixed &b) {
+  if (a < b)
+    return (a);
+  else
+    return (b);
 }
 
 std::ostream &operator<<(std::ostream &flux, const Fixed &fixed) {
--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 03:50:16 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/10/21 04:31:27 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/10/22 02:50:00 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ class Fixed {
 
 private:
   // Valeurs initial
-  int32_t _fixedVal;
+  int _fixedVal;
   const static int bits = 8;
 
 public:
@@ -30,30 +30,36 @@ public:
   Fixed(float number);
 
   // Comparator operator
+  bool operator>(const Fixed &other) const;
   bool operator<(const Fixed &other) const;
   bool operator>=(const Fixed &other) const;
   bool operator<=(const Fixed &other) const;
   bool operator==(const Fixed &other) const;
   bool operator!=(const Fixed &other) const;
+  Fixed &operator=(const Fixed &other);
 
   // Arithmetique
-  Fixed &operator+(const Fixed &other) const;
-  Fixed &operator-(const Fixed &other) const;
-  Fixed &operator*(const Fixed &other) const;
-  Fixed &operator/(const Fixed &other) const;
+  Fixed operator+(const Fixed &other);
+  Fixed operator-(const Fixed &other);
+  Fixed operator*(const Fixed &other);
+  Fixed operator/(const Fixed &other);
 
-  // Incrementation
-  Fixed &operator++();
-  Fixed &operator--();
-
-  // Desincrementation
+  // post Incrementation
   Fixed operator++(int);
   Fixed operator--(int);
+
+  // pre incrementation
+  Fixed &operator++();
+  Fixed &operator--();
 
   // Function members
   float toFloat(void) const;
   int32_t toInt(void) const;
   int getRawBits(void) const;
+  static Fixed max(Fixed &a, Fixed &b);
+  static Fixed max(const Fixed &a, const Fixed &b);
+  static Fixed min(Fixed &a, Fixed &b);
+  static Fixed min(const Fixed &a, const Fixed &b);
 
   // Destructor
   ~Fixed();
